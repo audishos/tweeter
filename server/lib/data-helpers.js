5446fa48b116ec16e22e54ef7a66e867dbe5738f
audishos <audisho.sada@gmail.com>
2017-09-21 11:14:25 -0400

    Adds graceful shudown (db.close()) attempt.

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let db;
+let db; // globally scope the db
 
 const {MongoClient} = require("mongodb");
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
@@ -14,6 +14,24 @@ MongoClient.connect(MONGODB_URI, (err, mongoInstance) => {
   // We have a connection to the "tweeter" db, starting here.
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 });
+
+// The code below here is to make sure that we close the conncetion to mongo when this node process terminates
+function gracefulShutdown() {
+  console.log("\nShutting down gracefully...");
+  try {
+    db.close();
+  }
+  catch (err) {
+    throw err;
+  }
+  finally {
+    console.log("I'll be back.");
+    process.exit();
+  }
+}
+
+process.on('SIGTERM', gracefulShutdown); // listen for TERM signal .e.g. kill
+process.on('SIGINT', gracefulShutdown);  // listen for INT signal e.g. Ctrl-C
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers() {
