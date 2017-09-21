d788f0cdebc1de6f9693e5ee0da1167b2111eca8
audishos <audisho.sada@gmail.com>
2017-09-21 11:12:03 -0400

    Moves mongo connection out of functions and globally scopes it.

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,49 +1,33 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
+let db;
+
+const {MongoClient} = require("mongodb");
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+MongoClient.connect(MONGODB_URI, (err, mongoInstance) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+  db = mongoInstance;
+  // We have a connection to the "tweeter" db, starting here.
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+});
 
 // Defines helper functions for saving and getting tweets, using the database `db`
-module.exports = function makeDataHelpers(db) {
+module.exports = function makeDataHelpers() {
   return {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      const {MongoClient} = require("mongodb");
-      const MONGODB_URI = "mongodb://localhost:27017/tweeter";
-
-      MongoClient.connect(MONGODB_URI, (err, db) => {
-        if (err) {
-          console.error(`Failed to connect: ${MONGODB_URI}`);
-          throw err;
-        }
-
-        // We have a connection to the "tweeter" db, starting here.
-        console.log(`Connected to mongodb: ${MONGODB_URI}`);
-        db.collection("tweets").insertOne(newTweet);
-        callback(null, true);
-        db.close();
-      });
+      db.collection("tweets").insertOne(newTweet, callback);
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      const {MongoClient} = require("mongodb");
-      const MONGODB_URI = "mongodb://localhost:27017/tweeter";
-
-      MongoClient.connect(MONGODB_URI, (err, db) => {
-        if (err) {
-          console.error(`Failed to connect: ${MONGODB_URI}`);
-          throw err;
-        }
-
-        // We have a connection to the "tweeter" db, starting here.
-        console.log(`Connected to mongodb: ${MONGODB_URI}`);
-        // const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        db.collection("tweets").find().toArray(callback);
-        db.close();
-      });
+      // const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      db.collection("tweets").find().toArray(callback);
     }
-
-  };
-}
+  }
+};
