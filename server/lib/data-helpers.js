87582a317324fc025aebe2756b65e1d9c128fffb
audishos <audisho.sada@gmail.com>
2017-09-20 20:02:00 -0400

    Refactors saveTweet to use mongo.

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,9 +9,20 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+      const {MongoClient} = require("mongodb");
+      const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+      MongoClient.connect(MONGODB_URI, (err, db) => {
+        if (err) {
+          console.error(`Failed to connect: ${MONGODB_URI}`);
+          throw err;
+        }
+
+        // We have a connection to the "tweeter" db, starting here.
+        console.log(`Connected to mongodb: ${MONGODB_URI}`);
+        db.collection("tweets").insertOne(newTweet);
         callback(null, true);
+        db.close();
       });
     },
 
@@ -28,7 +39,7 @@ module.exports = function makeDataHelpers(db) {
 
         // We have a connection to the "tweeter" db, starting here.
         console.log(`Connected to mongodb: ${MONGODB_URI}`);
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+        // const sortNewestFirst = (a, b) => a.created_at - b.created_at;
         db.collection("tweets").find().toArray(callback);
         db.close();
       });
